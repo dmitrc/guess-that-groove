328bf8585198a53af6a77a42e38537bca775d0fc
Sage Kitamorn <sagek@microsoft.com>
2017-07-24 19:21:39 -0700

    MOAR STRINGZ

--- strings/test.ts
+++ strings/test.ts
@@ -17,14 +17,48 @@ strings.sage2 =
     .say("I would like chinese")
     .toString();
 
-
+// Start of Game stuff
 strings.Intro =
     new ssml()
-    .say("Let's play Guess that Song!")
+    .say("OK, let's play Guess that Groove!")
     .pause("300ms")
     .say("Try to guess the following five Songs. You get one point for the artist or title, and three points if you get both. Let's begin!")
     .toString();
 
-strings.demo = strings.Intro
+// Cortana says these before playing a clip
+strings.PreClip1 =
+    new ssml()
+    .say("Here's the first one")
+    .toString();
 
+// Responses to correct guesses
+strings.CorrectArtist =
+    new ssml()
+    .say("You got the artist.")
+    .toString();
+
+strings.CorrectTitle =
+    new ssml()
+    .say("You got the song name.")
+    .toString();
+
+strings.CorrectArtistTitle =
+    new ssml()
+    .say("Whoa, nice! That's three points!")
+    .toString();
+
+// Responses to partially correct guesses
+strings.IncorrectArtist=
+    new ssml()
+    .say("That's not it.")
+    .toString();
+
+strings.IncorrectArtist=
+    new ssml()
+    .say("That's not it.")
+    .toString();
+// Response to a totally incorrect guess
+
+
+strings.demo = strings.Intro
 export default strings;
