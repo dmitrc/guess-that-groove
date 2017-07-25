a25d5878bf8f560613ffd35b8bb101fdcd354866
Sage Kitamorn <sagek@microsoft.com>
2017-07-24 18:30:26 -0700

    Adding demo strings

--- strings/test.ts
+++ strings/test.ts
@@ -3,11 +3,20 @@ import ssml from '../ssml';
 
 let strings: any = {};
 
-strings.demo =
+strings.sage =
     new ssml()
     .say("Hello!")
     .pause("330ms")
     .say("How are you doing?")
     .toString();
+
+strings.sage2 =
+    new ssml()
+    .say("What do you want for dinner?")
+    .pause("330ms")
+    .say("I would like chinese")
+    .toString();
+
+strings.demo = strings.sage2
 
 export default strings;
