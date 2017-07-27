1f3e2151b2394aa82d3561f98df9e7a41ff5758b
Qing Wang <t-qinw@microsoft.com>
2017-07-26 19:32:02 -0700

    simple website

--- static/leaderboard/main.js
+++ static/leaderboard/main.js
@@ -1,5 +1,25 @@
 var _w = window;
 
 _w.onload = function() {
-    console.log("It's alive!");
+    jQuery.ajax({
+            url: "api/leaderboard/1",
+            type: "GET",
+
+            contentType: 'application/json; charset=utf-8',
+            success: function(resultData) {
+                resultData = JSON.parse(resultData);
+
+                document.getElementById("name1").innerHTML = JSON.stringify(resultData[0]["user"]);
+                document.getElementById("score1").innerHTML = JSON.stringify(resultData[0]["score:"]);
+
+                document.getElementById("name2").innerHTML = JSON.stringify(resultData[1]["user"]);
+                document.getElementById("score2").innerHTML = JSON.stringify(resultData[1]["score:"]);
+
+                document.getElementById("name3").innerHTML = JSON.stringify(resultData[2]["user"]);
+                document.getElementById("score3").innerHTML = JSON.stringify(resultData[2]["score:"]);
+            },
+            error : function(jqXHR, textStatus, errorThrown) {
+            },
+            timeout: 120000,
+        });
 };
